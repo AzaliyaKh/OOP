9fb7267b8e3449e7ee688262d199ffd7be9c56d0
Azaliya <a_khakimyanova@mail.ru>
2026-02-27 21:27:51 +0500

    Updated class.cpp

--- laba_2/class.cpp
+++ laba_2/class.cpp
@@ -1,5 +1,4 @@
 #include <iostream> 
-#include <string>
 
 using namespace std;
 
@@ -9,80 +8,73 @@ class Device {
   enum Brand {noName, Apple, Samsung, Nokia, Xiomi};
   enum Software {noSoftware, IOS, Android, Huawei};
 
-  Device(): brand_(noName), model_(0000), color_(unknown), powerConsumption_(0), weight_(0), software_(noSoftware) {
-    cout << "Устройство создалось" << endl;
+  Device(): brand_(noName), model_(0000), color_(unknown) {
+    cout << "Device is created" << endl;
   }
 
-  Device(Brand brand, int model, Color color, int powerConsumption, int weight, Software software): 
-         brand_(brand), model_(model), color_(color), powerConsumption_(powerConsumption), weight_(weight), software_(software) {
-    cout << "Устройство создалось" << endl;
+  Device(Brand brand, int model, Color color): 
+         brand_(brand), model_(model), color_(color) {
+    cout << "Device is created" << endl;
   }
 
   Device(const Device& device) {
     brand_ = device.brand_;
     model_ = device.model_;
     color_ = device.color_;
-    powerConsumption_ = device.powerConsumption_;
-    weight_ = device.weight_;
-    software_ = device.software_;
-
-    cout << "Устройство создалось" << endl;
+    cout << "Device is created" << endl;
   }
 
   ~Device() {
-    cout << "Устройство устранилось. Пока!" << endl;
+    cout << "Device destroyed!" << endl;
   }
 
   virtual void TurnOn() {
-    cout << "Устройство включено!" << endl;
+    cout << "Device is turn on!" << endl;
   }
 
   virtual void TurnOff() {
-    cout << "Устройство выключено!" << endl;
+    cout << "Device is turn off!" << endl;
   }
 
  private:
   Brand brand_;
   int model_;
   Color color_;
-  int powerConsumption_;
-  int weight_;
-  Software software_;
 };
 
 
 class SmartPhone: public Device {
  public:
   SmartPhone(): Device(), SIMCard_(false), camMP_(0) {
-    cout << "Смартфон создался" << endl;
+    cout << "Smartphone is created" << endl;
   }
 
-  SmartPhone(Brand brand, int model, Color color, int powerConsumption, int weight, Software software, bool SIMCard, int camMP): 
-             Device(brand, model, color, powerConsumption, weight, software), SIMCard_(SIMCard), camMP_(camMP) {
-    cout << "Смартфон создался" << endl;
+  SmartPhone(Brand brand, int model, Color color, bool SIMCard, int camMP): 
+             Device(brand, model, color), SIMCard_(SIMCard), camMP_(camMP) {
+    cout << "Smartphone is created" << endl;
   }
 
   SmartPhone(const SmartPhone& smartPhone): Device(smartPhone) {
     SIMCard_ = smartPhone.SIMCard_;
     camMP_ = smartPhone.camMP_;
 
-    cout << "Смартфон создался" << endl;
+    cout << "Smartphone is created" << endl;
   }
 
   ~SmartPhone() {
-    cout << "Смартфон устранился. Пока!" << endl;
+    cout << "Smartphone is destroyed" << endl;
   }
 
   void TurnOn() override {
-    cout << "Смартфон включен!" << endl;
+    cout << "Smartphone is turn on!" << endl;
   }
 
   void TurnOff() override {
-    cout << "Смартфон выключен!" << endl;
+    cout << "Smartphone is turn off!" << endl;
   }
 
   virtual void Call() {
-    cout << "Звоню своей подружке Стейсииииии через телефон" << endl;
+    cout << "Calling my bestie Stacey on the phone" << endl;
   }
 
  private:
@@ -94,42 +86,49 @@ class SmartPhone: public Device {
 class Tablet: public SmartPhone {
  public:
   Tablet(): SmartPhone(), has_keyboard_(false) {
-    cout << "Планшет создался" << endl;
+    cout << "Tablet is created" << endl;
   }
 
-  Tablet(Brand brand, int model, Color color, int powerConsumption, int weight, 
-         Software software, bool SIMCard, int camMP, bool has_keyboard): 
-         SmartPhone(brand, model, color, powerConsumption, weight, software, SIMCard, camMP), 
+  Tablet(Brand brand, int model, Color color, bool SIMCard, int camMP, bool has_keyboard): 
+         SmartPhone(brand, model, color, SIMCard, camMP), 
          has_keyboard_(has_keyboard) {
-    cout << "Планшет создался" << endl;
+    cout << "Tablet is created" << endl;
   }
 
   Tablet(const Tablet& tablet): SmartPhone(tablet) {
     has_keyboard_ = tablet.has_keyboard_;
 
-    cout << "Планшет создался" << endl;
+    cout << "Tablet is created" << endl;
   }
 
   ~Tablet() {
-    cout << "Планшет устранился. Пока!" << endl;
+    cout << "Tablet is destroyed" << endl;
   }
 
   void TurnOn() override {
-    cout << "Планшет включен!" << endl;
+    cout << "Tablet is turn on!" << endl;
   }
 
   void TurnOff() override {
-    cout << "Планшет выключен!" << endl;
+    cout << "Tablet is turn off!" << endl;
   }
 
   void Call() override {
-    cout << "Звоню своей подружке Стейсииииии через планшет" << endl;
+    cout << "Calling my bestie Stacey on the tablet" << endl;
   }
 
   void Draw () {
-    cout << "Рисую портрет своего краша капитана футбольной команды Брендона";
+    cout << "Drawing a portrait of my crush, the captain of the football team, Brandon.";
   }
 
  private:
   bool has_keyboard_;
+};
+
+class CloudStorage {
+
+
+ private:
+  SmartPhone *ptr_smph;
+  Tablet *ptr_tblt;
 };
